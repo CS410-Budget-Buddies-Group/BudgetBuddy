b14c8dfa13860f608bfbf93aede1600afb270468
Sam DePoule <samdepoule@gmail.com>
2025-05-12 13:21:38 -0700

    started router

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as HashRouter, Routes, Route } from 'react-router-dom';
+import "./css/Router.css"
+
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+
+export default function Router() {
+
+	const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+	return (
+		<HashRouter basename='/IEMTracker'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+		</HashRouter>
+	);
+};
