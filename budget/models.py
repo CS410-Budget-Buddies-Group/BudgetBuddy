0f24261f567ccf8ceebf639c0bf520a3c35f2c66
Taylor Williams <taylor@thewilliams.net>
2025-06-09 15:36:14 -0700

    add validations to user model

--- budget/models.py
+++ budget/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(blank=False, max_length=25)
+    email = models.EmailField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name
+    
 
 class Job(models.Model):
     name = models.CharField(max_length=25)
