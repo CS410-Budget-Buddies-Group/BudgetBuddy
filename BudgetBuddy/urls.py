644f92566249681c8807add1a1532d32601046ce
Taylor Williams <taylor@thewilliams.net>
2025-06-09 16:18:16 -0700

    fix users validations

--- BudgetBuddy/urls.py
+++ BudgetBuddy/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from budget import views
+from budget.views import UserListView
 
 router = DefaultRouter()
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('users/', UserListView.as_view(), name='user-list'),
+]
